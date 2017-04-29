9cb4860baa60dcdf8f47196f7e2fe8d0b0d3a176
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-04-29 16:12:45 +0100

    add event listener and request summary function

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-var makers = 'http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=';
-var guardian = 'http://content.guardianapis.com';
+var makers = 'http://news-summary-api.herokuapp.com';
+var guardian = '/guardian?apiRequestUrl=http://content.guardianapis.com';
 var ukNews = '/uk-news';
+var aylien = '/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=';
 
-// apiGetter = new ApiGetter(makers + guardian + ukNews);
-// newsController = new NewsController(apiGetter.requestAndParseAPI());
-// newsController.createArticles();
-// newsController.renderHeadlines();
-
-
+apiGetter = new ApiGetter(makers + guardian + ukNews);
+newsController = new NewsController(apiGetter.requestAndParseAPI());
+newsController.createArticles();
+newsController.renderHeadlines();
 
 function makeUrlChange() {
   window.addEventListener('hashchange', showSummary);
 }
 
-function showCurrentNote() {
-  noteController.renderSingle(getNoteFromUrl(window.location));
+function showSummary() {
+  article = newsController._articleList._articles[getNoteFromUrl(window.location)];
+  apiGetter = new ApiGetter(makers + aylien + article.url());
+  console.log(apiGetter.requestAndParseAPI());
+  article.addSummary(apiGetter.requestAndParseAPI());
+
 }
 
 function getNoteFromUrl(location) {
   return location.hash.split('headline_')[1];
 }
 
+makeUrlChange();
+
 });
 
 
 
-//newsController._articleList._articles.forEach(function(article) {
-//   apiGetter = new ApiGetter(makers + aylien + article.url());
-//   console.log(apiGetter.requestAndParseAPI());
-//   article.addSummary(apiGetter.requestAndParseAPI());
+
 
 //summary = document.getElementById('summary_0');
 
