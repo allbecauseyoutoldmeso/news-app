2f557db022ba5b7ccac4cacebfa16dc6edd34f40
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-04-29 23:17:03 +0100

    terrible css

--- spec/article-list-view-spec.js
+++ spec/article-list-view-spec.js
@@ -24,7 +24,7 @@ function testRenderHeadlines() {
   var articleList = new ArticleListDouble();
   articleListView = new ArticleListView(articleList);
   articleListView.renderHeadlines();
-  assert.toEqual(document.getElementById('headlines').innerHTML, '<p><a href="#headline_0">Cat rescued from tree</a></p><a href="#summary_0"><p id="summary_0"></p></a><p><a href="#summary_0"></a></p><p id="text_0"></p><p></p><p><a href="#headline_1">Grandma wins lottery</a></p><a href="#summary_1"><p id="summary_1"></p></a><p><a href="#summary_1"></a></p><p id="text_1"></p><p></p>');
+  assert.toEqual(document.getElementById('headlines').innerHTML, '<p class="headline"><a href="#headline_0">Cat rescued from tree</a></p><a href="#summary_0"><p class="summary" id="summary_0"></p></a><p><a href="#summary_0"></a></p><p class="text" id="text_0"></p><p></p><p class="headline"><a href="#headline_1">Grandma wins lottery</a></p><a href="#summary_1"><p class="summary" id="summary_1"></p></a><p><a href="#summary_1"></a></p><p class="text" id="text_1"></p><p></p>');
 }
 
 testArticleListViewHeadlines();
