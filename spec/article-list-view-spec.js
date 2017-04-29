ef6d998f55ee5c29b853fff6e9ad623eb15dbed1
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-04-29 18:19:27 +0100

    updated tests and everything works

--- spec/article-list-view-spec.js
+++ spec/article-list-view-spec.js
@@ -9,7 +9,7 @@ function testArticleListViewHeadlines() {
   };
   var articleList = new ArticleListDouble();
   articleListView = new ArticleListView(articleList);
-  assert.toEqual(articleListView.viewHeadlines(), "<p><a href='#headline_0'>Cat rescued from tree</a></p><p id='summary_0'><p>");
+  assert.toEqual(articleListView.viewHeadlines(), "<p><a href='#headline_0'>Cat rescued from tree</a></p><a href='#summary_0'><p id='summary_0'><p></a><p id='text_0'><p>");
 }
 
 function testRenderHeadlines() {
@@ -24,7 +24,7 @@ function testRenderHeadlines() {
   var articleList = new ArticleListDouble();
   articleListView = new ArticleListView(articleList);
   articleListView.renderHeadlines();
-  assert.toEqual(document.getElementById('headlines').innerHTML, '<p><a href="#headline_0">Cat rescued from tree</a></p><p id="summary_0"></p><p></p><p><a href="#headline_1">Grandma wins lottery</a></p><p id="summary_1"></p><p></p>');
+  assert.toEqual(document.getElementById('headlines').innerHTML, '<p><a href="#headline_0">Cat rescued from tree</a></p><a href="#summary_0"><p id="summary_0"></p></a><p><a href="#summary_0"></a></p><p id="text_0"></p><p></p><p><a href="#headline_1">Grandma wins lottery</a></p><a href="#summary_1"><p id="summary_1"></p></a><p><a href="#summary_1"></a></p><p id="text_1"></p><p></p>');
 }
 
 testArticleListViewHeadlines();
