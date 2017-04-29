7175eeb914085eb2c6e770a37c312af7712ff051
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-04-29 13:46:52 +0100

    all works

--- src/news-controller.js
+++ src/news-controller.js
@@ -3,13 +3,15 @@
   function NewsController(jsonObj) {
     this._results = jsonObj.response.results;
     this._articleList = new ArticleList();
+    this._articleId = 0;
   }
 
   NewsController.prototype.createArticles = function() {
     for(x=0; x<10; x++) {
-      var thisArticle = new Article(this._results[x].webTitle);
+      var thisArticle = new Article(this._results[x].webTitle, this._articleId);
       thisArticle.addUrl(this._results[x].webUrl);
       this._articleList.storeArticle(thisArticle);
+      this._articleId += 1;
     }
   };
 
