39a19c39896551a34f090e5f2a16099d5ee4f866
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-04-29 14:43:07 +0100

    add div for summaries

--- spec/article-list-view-spec.js
+++ spec/article-list-view-spec.js
@@ -9,7 +9,7 @@ function testArticleListViewHeadlines() {
   };
   var articleList = new ArticleListDouble();
   articleListView = new ArticleListView(articleList);
-  assert.toEqual(articleListView.viewHeadlines(), "<p><a href='headline_0'>Cat rescued from tree</a></p>");
+  assert.toEqual(articleListView.viewHeadlines(), "<p><a href='headline_0'>Cat rescued from tree</a></p><p id='summary_0'><p>");
 }
 
 function testRenderHeadlines() {
@@ -24,7 +24,7 @@ function testRenderHeadlines() {
   var articleList = new ArticleListDouble();
   articleListView = new ArticleListView(articleList);
   articleListView.renderHeadlines();
-  assert.toEqual(document.getElementById('headlines').innerHTML, '<p><a href="headline_0">Cat rescued from tree</a></p><p><a href="headline_1">Grandma wins lottery</a></p>');
+  assert.toEqual(document.getElementById('headlines').innerHTML, '<p><a href="headline_0">Cat rescued from tree</a></p><p id="summary_0"></p><p></p><p><a href="headline_1">Grandma wins lottery</a></p><p id="summary_1"></p><p></p>');
 }
 
 testArticleListViewHeadlines();
