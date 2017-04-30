7d94b984c2b51899dcaf4da4eb5f6dc89690237c
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-04-30 09:19:13 +0100

    tiny refactor

--- src/news-controller.js
+++ src/news-controller.js
@@ -3,15 +3,14 @@
   function NewsController(jsonObj) {
     this._results = jsonObj.response.results;
     this._articleList = new ArticleList();
-    this._articleId = 0;
+
   }
 
   NewsController.prototype.createArticles = function() {
     for(x=0; x<10; x++) {
-      var thisArticle = new Article(this._results[x].webTitle, this._articleId);
+      var thisArticle = new Article(this._results[x].webTitle, x);
       thisArticle.addUrl(this._results[x].webUrl);
       this._articleList.storeArticle(thisArticle);
-      this._articleId += 1;
     }
   };
 
@@ -35,7 +34,7 @@
   };
 
   NewsController.prototype.renderText = function(id) {
-    this._articleListView.renderText(id);  
+    this._articleListView.renderText(id);
   };
 
   exports.NewsController = NewsController;
